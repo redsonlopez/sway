93c3ba75ac139e1f83e24ff147fc8ed6d2a31b8c
redsonlopez <redsonlopez@gmail.com>
2024-09-20 23:00:17 -0300

    try btrfs

---
+++ arch/backup_btrfs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SNAPSHOT_DIR="/@.snapshots"
+BACKUP_DIR="/mnt/backup/snapshots"
+
+sudo btrfs send $SNAPSHOT_DIR/root_snapshot_ | sudo btrfs receive $BACKUP_DIR
+sudo btrfs send $SNAPSHOT_DIR/home_snapshot_ | sudo btrfs receive $BACKUP_DIR
+sudo btrfs send $SNAPSHOT_DIR/log_snapshot_ | sudo btrfs receive $BACKUP_DIR
+sudo btrfs send $SNAPSHOT_DIR/pkg_snapshot_ | sudo btrfs receive $BACKUP_DIR
+sudo btrfs send $SNAPSHOT_DIR/snapshots_snapshot_ | sudo btrfs receive $BACKUP_DIR
